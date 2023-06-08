f059431dd3296dac1626a5c1328130e9bc7f024d
jose <joseerahe@gmail.com>
2023-06-07 18:02:20 -0700

    finished

--- index.js
+++ index.js
@@ -25,6 +25,23 @@ const cosasQueAprendimos = [
   },
 ];
 
-function main() {}
+
+
+
+function main() {
+  let listaOriginal = document.querySelectorAll('.lista li')
+ let nuevaLista = document.querySelector('.lista')
+  
+  for (const element of listaOriginal) {
+    element.remove()
+  }
+  for (const newElement of cosasQueAprendimos) {
+    let li = document.createElement('li')
+    li.textContent = newElement.tema
+    li.classList.add(newElement.class || 'item')
+    nuevaLista.appendChild(li)
+  }
+  
+}
 
 main();
